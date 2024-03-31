65093b255a1ee7599d9918c11de626c7ea3f73f5
saumyaoj04 <150843330+saumyaoj04@users.noreply.github.com>
2024-04-01 01:40:14 +0530

    signup

---
+++ app/src/main/java/com/example/ecell/onBoard2.kt
@@ -0,0 +1,11 @@
+package com.example.ecell
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class onBoard2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_board2)
+    }
+}
