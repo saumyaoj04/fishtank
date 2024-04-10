76c6f955ffbe0234d6dd1fbf4f3b1fe7ea37922f
saumyaoj04 <150843330+saumyaoj04@users.noreply.github.com>
2024-04-11 02:41:04 +0530

    Signup and login implementation

--- app/src/main/java/com/devshiv/fishtankecell/activity/LoginActivity.kt
+++ app/src/main/java/com/devshiv/fishtankecell/activity/LoginActivity.kt
@@ -1,25 +1,53 @@
 package com.devshiv.fishtankecell.activity
 
+import android.content.ContentProviderClient
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import com.devshiv.fishtankecell.R
 import com.devshiv.fishtankecell.databinding.ActivityLoginBinding
 import com.devshiv.fishtankecell.databinding.ActivitySplashBinding
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityLoginBinding
 
+    lateinit var binding: ActivityLoginBinding
+    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var googleSignInClient:GoogleSignInAccount
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        firebaseAuth=FirebaseAuth.getInstance()
+        val gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.))
         binding.signUpTxt.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
+
+        }
+        binding.button.setOnClickListener {
+            val email=binding.emailET.text.toString()
+            val pass=binding.passwordET.text.toString()
+
+            if(email.isNotEmpty()&&pass.isNotEmpty())
+            firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener{
+                if(it.isSuccessful){
+                    val int=Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(this,it.exception.toString() , Toast.LENGTH_SHORT).show()
+                }
+
+            }else{
+                Toast.makeText(this,"Empty fields are not allowed",Toast.LENGTH_SHORT)
+            }
+
         }
 
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
--- app/src/main/java/com/devshiv/fishtankecell/activity/SignUpActivity.kt
+++ app/src/main/java/com/devshiv/fishtankecell/activity/SignUpActivity.kt
@@ -3,27 +3,46 @@ package com.devshiv.fishtankecell.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import carbon.widget.RelativeLayout
 import com.devshiv.fishtankecell.R
 import com.devshiv.fishtankecell.databinding.ActivityLoginBinding
 import com.devshiv.fishtankecell.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
 
     lateinit var binding: ActivitySignupBinding
+    lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        firebaseAuth=FirebaseAuth.getInstance()
 
         binding.signInTxt.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
 
         binding.continueBtn.setOnClickListener {
+            val email=binding.emailET.text.toString()
+            val pass=binding.passwordET.text.toString()
+
+            if(email.isNotEmpty()&&pass.isNotEmpty())
+                firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val int=Intent(this,MainActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this,it.exception.toString() , Toast.LENGTH_SHORT).show()
+                    }
+
+                }else{
+                Toast.makeText(this,"Empty fields are not allowed", Toast.LENGTH_SHORT)
+            }
             startActivity(Intent(this, MainActivity::class.java))
         }
 
